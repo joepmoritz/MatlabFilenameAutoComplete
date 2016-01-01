569b60fecade7934b9aca4fb2965cab011f2ab75
Junyuan Hong <jyhong836@gmail.com>
2016-01-01 14:51:55 +0800

    Make the trigger more fancy. Fix some bugs. Make more efficient.

--- MatlabFilenameAutoComplete.py
+++ MatlabFilenameAutoComplete.py
@@ -5,21 +5,19 @@ class MatlabFilenameAutoComplete(sublime_plugin.EventListener):
 	def __init__(self):
 		super(MatlabFilenameAutoComplete, self).__init__()
 		self.max_depth = 8
+		self.depth = 0
 
-		self.fun_reg = re.compile(r'function.+[a-zA-Z]+[a-zA-Z0-9_]*\(.*\).*')
-		self.fun_line_reg = re.compile(r'[a-zA-Z]+[a-zA-Z0-9_]*\(.*\)')
+		self.fun_reg = re.compile(r'function.+[\s=]([a-zA-Z]\w*\s*\(.*\))')
 		self.param_reg = re.compile(r'[\(,]?\s*([a-zA-Z]+[a-zA-Z0-9_]*)\s*[\),]?')
 
 	def on_query_completions(self, view, prefix, locations):
 		scope = view.scope_name(view.sel()[0].begin());
-
 		if not scope.startswith('source.matlab'):
 			return None
 
 		completions = []
 		for folder in sublime.active_window().folders():
-			self.depth = 0
-			completions.extend(self.__get_completions(folder))
+			completions += self.__get_completions(folder)
 
 		return completions
 
@@ -29,26 +27,24 @@ class MatlabFilenameAutoComplete(sublime_plugin.EventListener):
 		for f in os.listdir(folder):
 			if f.startswith('.'): continue
 			full_f = os.path.join(folder, f)
-			if os.path.isfile(full_f) and f.endswith( ('.m','.dll','.mexa64','mexmaci64','mexw32') ): 
-				trigger = f[:-2]
-				content = trigger
-				if f.endswith('.m'):
+			[f, ext] = os.path.splitext(f)
+			if os.path.isfile(full_f) and ext in ('.m','.mexa64','.mexmaci64','.mexw32'): 
+				(trigger, content) = (f, f)
+				if ext == '.m':
 					with open(full_f, 'r') as fh:
-						rdstr = fh.readline()
-						if self.fun_reg.match(rdstr):
-							m = self.fun_line_reg.search(rdstr)
-							func = m.group()
-							params = self.param_reg.findall(func)
-							content = params[0] + '('
+						func = self.fun_reg.findall(fh.readline())
+						if len(func) > 0:
+							params = self.param_reg.findall(func[0])
+							trigger += '('
+							content += '('
 							nParam = len(params)
 							for ii in range(1,nParam):
 								content += '${' + str(ii) + ':' + params[ii] + '}' + (', ' if ii!=nParam-1 else '')
+								trigger += params[ii] + (', ' if ii!=nParam-1 else '')
 							content += ')'
+							trigger += ')'
 				completions.append([trigger, content])
-			else:
-				if os.path.isdir(full_f) and self.depth < self.max_depth:
-					completions.extend(self.__get_completions(full_f))
+			elif os.path.isdir(full_f) and self.depth < self.max_depth:
+				completions += self.__get_completions(full_f)
 		self.depth -= 1
-		if completions.count == 0:
-			return None
 		return completions
